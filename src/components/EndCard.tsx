c15b8a85c8192e4a17f0f396d6d5016572ec358a
Simón Bustamante Alzate <simonba97@hotmail.com>
2024-01-03 18:57:39 -0500

    Configuración para despliegue en Netlify

--- src/components/EndCard.tsx
+++ src/components/EndCard.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const EndCard = () => {
     return (
         <div>EndCard</div>
--- src/components/HelpCard.tsx
+++ src/components/HelpCard.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const HelpCard = () => {
     return (
         <div>HelpCard</div>
