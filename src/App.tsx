cf9e9df78504930085314bc7713c7168a44e412c
Simón Bustamante Alzate <simonba97@hotmail.com>
2024-01-03 19:41:32 -0500

    Corrección para el despliegue de Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PreBirthday from './components/PreBirthday';
 import StartCard from './components/StartCard';
 import EndCard from './components/EndCard';
