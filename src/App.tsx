8fc542b98256be5681bbf416870f2deae7d6f1a6
Simón Bustamante Alzate <simonba97@hotmail.com>
2024-01-13 01:39:08 -0500

    birthday card done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PreBirthday from './components/PreBirthday';
-import StartCard from './components/StartCard';
-import EndCard from './components/EndCard';
-import HelpCard from './components/HelpCard';
+import BirthdayCard from './components/BirthdayCard';
 
 const App = () => {
   return (
@@ -10,9 +8,7 @@ const App = () => {
       <div>
         <Routes>
           <Route path="/pre-birthday" element={<PreBirthday />} />
-          <Route path="/start-card" element={<StartCard />} />
-          <Route path="/end-card" element={<EndCard />} />
-          <Route path="/help-card" element={<HelpCard />} />
+          <Route path="/birthday-card" element={<BirthdayCard />} />
         </Routes>
       </div>
     </Router>
--- src/components/BirthdayCard.tsx
+++ src/components/BirthdayCard.tsx
@@ -1,4 +1,4 @@
-const StartCard = () => {
+const BirthdayCard = () => {
     return (
         <section className="w-full h-screen flex items-center justify-center bg-gradient-to-tr from-emerald-200 via-sky-200 to-pink-200">
             <div className="w-[90%] flex-row text-center font-Oswald">
@@ -6,11 +6,13 @@ const StartCard = () => {
 
                     <div>
                         <div className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-gradient-to-r from-fuchsia-500 to-cyan-500 relative inline-block">
-                            <span className="relative text-white">{"LAURA'S BIRTHDAY"}</span>
+                            <span className="relative text-white">{"YO NACÍ PARA AMAR"}</span>
                         </div>
                     </div>
                     <div>
-                        <span>{'ESTÁ POR COMENZAR'}</span>
+                        <span>{'MI FLAQUITA, GRACIAS POR CRUZARTE EN MI CAMINO. HOY CELEBRAMOS EN EL CIELO Y LA TIERRA LA INMENSA FORTUNA QUE ES TENERTE A NUESTRO LADO.'}</span>
+                        <br />
+                        <span>{'ESPERO DISFRUTES DE ESTE REGALO POR EL RESTO DE TU VIDA'}</span>
                     </div>
                 </div>
                 <div className="text-5xl font-semibold m-auto bg-gradient-to-r from-fuchsia-500 to-cyan-500 bg-clip-text text-transparent">
@@ -21,4 +23,4 @@ const StartCard = () => {
     );
 }
 
-export default StartCard
+export default BirthdayCard
